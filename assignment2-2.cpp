2c4a77e20215bcf0530667eb3242cc0bbba8a188
ZACHARIAH KOVAC <zkovac12@icloud.com>
2021-01-25 21:17:10 +0000

    Submitted Assignment

---
+++ assignment2-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std; 
+
+int main()
+{
+
+ char c;
+
+ c = 90;
+ cout << c ;
+
+ c = 65;
+ cout << c ;
+ 
+ c = 67;
+ cout << c ;
+ 
+ c = 75;
+ cout << c << "\n";
+
+}
