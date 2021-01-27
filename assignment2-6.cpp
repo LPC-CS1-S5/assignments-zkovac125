2aaa7b19576df29c4f0d271f3c955fb6e778796c
ZACHARIAH KOVAC <zkovac12@icloud.com>
2021-01-27 23:28:32 +0000

    Submitted Assignment

--- assignment2-6.cpp
+++ assignment2-6.cpp
@@ -1,4 +1,4 @@
-//Write a program that  stores the following values in five different variables:
+//Write a program that stores the following values in five different variables:
 
 //28, 32, 37, 24, and 33.
 
@@ -11,3 +11,16 @@ using namespace std;
 
 int main()
 {
+  double num1 = 28;
+  double num2 = 32;
+  double num3 = 37;
+  double num4 = 24;
+  double num5 = 33;
+  double sum = num1 + num2 + num3 + num4 + num5;
+  double average = sum / 5;
+  
+
+  cout << "Sum: " << sum << endl;
+  cout << "Average: " << average << endl;
+  
+}
