7a49ec660b3e76fa7a593c3cbf7354e30ec3a354
ZACHARIAH KOVAC <zkovac12@icloud.com>
2021-02-05 19:19:49 +0000

    Submitted Assignment

--- Chapter 3 Assignments/assignment3-3.cpp
+++ Chapter 3 Assignments/assignment3-3.cpp
@@ -10,7 +10,7 @@ int main()
 
   cin >> celcius;
 
-  fahrenheit = 9/5.0 * celcius + 32;
+  fahrenheit = (9/5.0) * celcius + 32;
 
   cout << celcius << " Celcius" << setprecision(3) << " is " << fahrenheit << setprecision(3) << " Fahrenheit\n";
   
--- assignment3-5.cpp
+++ assignment3-5.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-#include <cstlib>
+#include <cstdlib>
 #include <ctime>
 using namespace std;
 
 int main()
 {
-  unsigned seed = time (0);
-  srad(seed);
+  int num1, num2, num3;
+  int sum;
+  int average;
+  srand(time(0));
 
-  cout << rand() % 100;
-  cout << rand() %
+  num1 = rand() % 100;
+  num2 = rand() % 100;
+  num3 = rand() % 100;
 
+  sum = num1 + num2 + num3;
+  average = sum/3;
+  
+  cout << "The random number 1 is " << num1 <<endl;
+  cout << "The random number 2 is " << num2 <<endl;
+  cout << "The random number 3 is " << num3 <<endl;
+  cout << "The sum is " << sum <<endl;
+  cout << "The average is " << average <<endl;
 
 }
