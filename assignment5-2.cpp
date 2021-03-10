4f44fd422e6ea480009cb4280e782e8c65a8b3b6
ZACHARIAH KOVAC <zkovac12@icloud.com>
2021-03-10 01:44:44 +0000

    submitted Assignment

--- assignment5-2.cpp
+++ assignment5-2.cpp
@@ -2,44 +2,86 @@
 
 using namespace std;
  
+//int main()
+//{
+    //int checkPrimeNumber (int);
+    //int num, begin, end, i, flag;
+ 
+    //cout << "Input two integers: ";
+    //cin >> begin >> end;
+    //num = begin; 
+    
+    //if (num > end){
+      //end = num + end;
+      //num = end - num;
+      //end = end -num;
+    //}
+    //cout << "Prime numbers are between " << num << " and " << end << " are: ";
+
+    //for (int i = num+1; i < end; ++i) {
+     // flag = checkPrimeNumber(i);
+
+     // if (flag)
+      //  cout << i << " ";
+  //  }
+//}
+
+//int checkPrimeNumber(int n){
+  //bool isPrime = true;
+
+  //if (n == 0 || n == 1) {
+   // isPrime = false;
+  //}
+  //else {
+    //for (int j = 2; j <= n/2; ++j){
+     // if (n%j == 0){
+      //  isPrime = false;
+       // break;
+     // }
+    //}
+  //}
+  //return isPrime;
+//}
+///int main ()
+//{
+  //int numStudent, numScore;
+  //string stuName;
+  //double score, sum, average;
+
+
+  //cout << "Enter the number of students and score: ";
+  //cin >> numStudent >> numScore;
+
+  //for (int i = 0; i < numStudent; i++)
+  //  cout << "Enter the students name : ";
+  //{
+    //cin >> stuName;
+    //cout << "Student's name : " << stuName << "\t";
+    //sum = 0;
+    //for (int j = 0; j < numScore; j++)
+    //{
+      //cout << "Enter the student's score : ";
+      //cin >> score;
+      //cout << "Score " << j + 1 << " : " << score << endl;
+      //sum += score;
+    //}
+   // average = sum / numScore;
+    //cout << "Sum of scores " << sum << "\t";
+    //cout << "Average of scores " << average << "\n";
+  //}
+//}
+
+
 int main()
 {
-    int begin, end, i=0, j= 0, sum, flag;
- 
-    cout << "Input two integers: ";
-    cin >> begin >> end;
+    int i, j;
 
-    cout << "\nPrime numbers between " << begin << " and " << end << " are: ";
+    cout << "Enter number of rows: ";
+    cin >> rows;
 
-    if (begin > end)
-    {
-      cout << "Input two integers: ";
-      cin >> begin >> end;
+    for(int i;;){
+      for(intj;;)
+           cout << i << ',' << j << endl;
     }
-  
-    for(i=begin; i<end; i++) {
-        if (i == i || i == 0)
-         continue;
-        flag = 1;
-        for(j=2; j< (i/2); j++) {
-         if (i % j == 0)
-          flag = 0;
-          break;
-      }
-    }
-      if (flag == 1) 
-      {
-        cout << i << " ";
-      } 
- return 0;
 }
-     
-  
- 
-  
- 
-  
- 
- 
-        
-
+    
