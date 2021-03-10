220970066b68a7d6101d3fb70ec902052299224d
Kyuwoong Lee <kwooler@gmail.com>
2021-03-10 17:02:53 +0000

    Kyu

---
+++ q5-2q1.cpp
@@ -0,0 +1,29 @@
+//question 1
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int main ()
+{
+  int num, min;
+  int N, i;
+  int rdnum;
+ 
+  srand(time(0));
+  
+  
+  for ( int i=0; i < N; i++)
+  {
+    num = rand() % 100;
+    min = num;
+    if(min<num)	
+  }
+
+
+  
+  cout << "The Radnom Generated Numbers are: " << rdnum << endl;
+  cout << "The min value is" << min << endl;
+
+
+}
---
+++ q5-2q2.cpp
@@ -0,0 +1,32 @@
+using namespace std;
+
+int main()
+{
+  int base;
+  int exp;
+  int power_Number;
+  int i;
+  int end;
+
+  cout << "Enter the base and the power nu,ber please: ";
+  cin >> base >> exp;
+
+  i = 0;
+  power_Number = 1;
+  while( i < exp )
+  {
+    power_Number = power_Number * base;
+    i++;
+  }
+  cin >> end;
+  power_Number = 1;
+
+  while(power_Number < start){
+    power_Number += 2;
+  }
+  while( power_Number < end){
+    power_Number = power_Number
+  }
+}
+//question 3
+//question 4
