d7140ebcb8a87c910ab2f5b0cd5728327941c9f1
ZACHARIAH KOVAC <zkovac12@icloud.com>
2021-03-31 15:51:23 +0000

    commit

--- Midterm/ques2.cpp
+++ Midterm/ques2.cpp
@@ -4,19 +4,19 @@
 
 using namespace std;
 
-int N,i,j;
-string stuName;
-double score_1, score_2, average, score_Sum1 = 0, classSum=0, classSum= 0, classAvg= 0;
-ofstream ofs;
-
-ofs.open("student.text");
-if ((ofs.fail())
-{
-  
-}
-
 int main()
 {
+  int N,i,j;
+  string stuName;
+  double score_1, score_2, average, score_Sum1 = 0, classSum=0, classSum= 0, classAvg= 0;
+  ofstream ofs;
 
+  ofs.open("student.text");
+    if ((ofs.fail())
+  {
+    cerr << "Open Error" << endl;
+    exit(0);
+  }
+  cout << "Enter the number of students: " << endl;
+  cin >>
 }
-
