30088a5ddfe3f8cb9be706c93614353b47bbc8b8
ZACHARIAH KOVAC <zkovac12@icloud.com>
2021-03-31 15:48:34 +0000

    commit

---
+++ Midterm/ques2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int N,i,j;
+string stuName;
+double score_1, score_2, average, score_Sum1 = 0, classSum=0, classSum= 0, classAvg= 0;
+ofstream ofs;
+
+ofs.open("student.text");
+if ((ofs.fail())
+{
+  
+}
+
+int main()
+{
+
+}
+
