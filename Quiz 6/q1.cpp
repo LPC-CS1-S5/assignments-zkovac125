bae92bfed296923b4872033bcd4ae0a5a10c6aa0
ZACHARIAH KOVAC <>
2021-04-13 19:08:51 +0000

    commit

--- Quiz 6/q1.cpp
+++ Quiz 6/q1.cpp
@@ -2,14 +2,25 @@
 
 using namspace std;
 
-int myFunction(int,int)
+void swapTwoValues(int,int)
+void swapTwoFloatValues(int,int)
 
 main()
 {
-  int num1 = 
-  int num2 =
+  //test if the user values are integer or int num1 = 
+  int num1
+  int num2
+  cout << "Enter the two numbers" endl;
+  cin >> num1 >> num2 >> endl;
+  cin.get(ch);
+  if(ch== '.')
 
+  if(num1 || num2)
 
 
 }
-swapTwoValues()
+void swapTwoValues(int n1, int n2)
+{
+
+}
+void swapTwoValues(float f1, float f2)
