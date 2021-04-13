7c455a90e0c674f5826deedcc978acd585ac1c88
ZACHARIAH KOVAC <>
2021-04-13 20:59:50 +0000

    commit

--- q2.cpp
+++ q2.cpp
@@ -13,17 +13,16 @@ int main()
 {
   int rdnum;
   srand(time(0));
-  for (int i = 0; i < N; i++) {
+  do (int i = 0; i < N; i++) {
    rdnum = getRdnum(n1, n2, n3);
-   cout << " The random number are : "<< n1 << n2 << n3 << endl;
-  if (findMin(rdnum)); {
+  }
+  while (findMin(rdnum)); {
     min = findMin(n1, n2, n3);                    
-                }
+  }
   
   printResult(n1, n2, n3, min);
 }
 void getRandnum(int n1, int n2, int n3){
-  //In this function, generate three random numbers and assign the values to the parameter n1, n2, and n3
    rand() % 100 + 1 = n1;
    rand() % 100 + 1 = n2;
    rand() % 100 + 1 = n3;
@@ -37,18 +36,18 @@ int  findMin(int n1, int n2, int n3){
                 prec_num = n;
                 return 0;
         }
-        if (prec_num < n) {
+        if (prec_num > n) {
                 prec_num = n;
-                return 1;
+                return min;
         } else {
                 prec_num = n;
                 return 0;
         }
-  return
 
 }
 void printResult(int n1, int n2, int n3, int min)
 {
  cout << "the random number are: " << n1 << n2 << n3 endl;
  cout << "The minium number is: " << min << endl;
+ return 0;
 }
