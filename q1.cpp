029739401799e376a50169696629cfbb5212c79e
ZACHARIAH KOVAC <>
2021-04-13 19:56:40 +0000

    commit

--- q1.cpp
+++ q1.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 
-using namspace std;
+using namespace std;
 
-void swapTwoValues(int&,int&)
-void swapTwoFloatValues(int&,int&)
+int num1;
+int num2;
+char ch1, ch2;
+int f1;
+int f2;
 
-main()
+void swapTwoValues(int &,int &);
+void swapTwoFloatValues(float &,float &);
+
+int main()
 {
-  //test if the user values are integer or int num1 = 
-  int num1 =0;
-  int num2 =0;
-  char ch1;
-  char ch2;
-  cout << "Enter the two numbers" endl;
+  //test if the user values are integer or int num1 
+  cout << "Enter the two numbers" << endl;
   cin >> num1 >> num2 >> endl;
+  num1 = ch1;
+  num2 = ch2;
   cin.get(ch1);
   if(ch1== '.'){
-    num1 = f1
+    ch1 = f1;
     swapTwoFloatValues(f1,f2);
     cout << "The new first number is: " << f1 << endl;
   }else{
-    num2= n1;
+    ch1= n1;
     swapTwoValues(n1,n2);
     cout << "The new first number is: " << n2 << endl;
   }
   cin.get(ch2);
   if(ch2== '.'){
-    num2 = f2;
+    ch2 = f2;
     swapTwoFloatValues(f1,f2);
     cout << "The new second numbers is: " << tnp << endl;
   }else{
-    num2 = n2;
+    ch2 = n2;
     swapTwoValues(n1,n2);
     cout << "The second numbers is: " << tnp << endl;
   }
@@ -39,14 +43,18 @@ main()
 void swapTwoValues(int n1, int n2)
 {
   int tnp;
-  tap = n1;
+  tnp = n1;
   n1 = n2;
   n2 = tnp;
+  retun n2;
+  return n1;
 }
 void swapTwoFloatValues(float f1, float f2)
 {
   int tnp;
-  tap = f1;
+  tnp = f1;
   f1 = f2;
   f2 = tnp;
+  return f1;
+  return f2;
 }
