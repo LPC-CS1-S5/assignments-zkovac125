db1b6ed6a8a26ca6d69dd30a85fcfc6daf84f1be
ZACHARIAH KOVAC <>
2021-04-30 19:25:03 +0000

    Save

--- lab7-7.cpp
+++ lab7-7.cpp
@@ -3,32 +3,34 @@
 #include <iostream>
 using namespace std;
 
-int finduserstring(char[], int, char[], int);
+int finduserstring(char [], int, char [], int);
 int main() {
-        int cstrlen, userlen, position;
+        int baselen, userlen, position;
         char cstr[] = "Chocolate";
         char userstr[100];   
 
-        cstrlen = cstrlen(cstr);
-        cin >> userstr; //* gets the users input*//
-        userlen = cstrlen(userstr);
+        baselen = sizeof(cstr);
+        cout << cstr << endl;
+        cout << "Enter the string to find\n";
+        cin.getline(userstr,100); //* gets the users input*//
+        userlen = cin.gcount();
 
-        position = finduserstring(cstr, cstrlen, userstr, userlen); //* Calls the function*//
+        position = finduserstring(cstr, baselen, userstr, userlen); //* Calls the function*//
         if (position != -1)
                 cout << "Found your string at : " << position << endl;
         else
                 cout << "Not found\n";
 }
-int finduserstring(char cstr[], int cstrlen, char userstr[], int userlen) 
+int finduserstring(char cstr[], int baselen, char userstr[], int userlen) 
 {
   int j, pos=-1;
 
-  for(int i-=0; i<cstrlen-userlen+1; i++){
+  for(int i=0; i<baselen-userlen+1; i++){
     for(j=0; j<userlen; j++){ //*number of c string lengths*//
       if ( cstr[i+j] != userstr[j] ) //* If two char are different it exits the loop
         break;
     {
-      if(j== userlen) { //* User string matches the c string*//
+      if(j==userlen) { //* User string matches the c string*//
         pos =1;
         return pos;
       }
