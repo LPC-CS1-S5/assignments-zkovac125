0daa123e7674d0924b147555c3a2e7773d3f8cf1
ZACHARIAH KOVAC <>
2021-05-04 19:12:26 +0000

    commit

--- q1.cpp
+++ q1.cpp
@@ -4,17 +4,18 @@
 using namespace std;
 
 const int N = 10;
-void checkcond (int arr[], int);
+void checkcond (a, n);
 
 int main()
 {
   int number[N] = {10, 15, 0, 19, 20, 25, 30, 45, 20};
-  checkcond(N, i);
+  int i;
+  checkcond(int arr[], i);
   return 0;
 }
 void checkcond(int arr[], int N)
 {
-  for(i=0; i<N; i++){
+  for(int i=0; i<N; i++)
    if(abs(N[i] - N[i + 1]) < 10)
     cout << "Less than 10\n";
 }
