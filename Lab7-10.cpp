2e0ffbcced2de8e647b8d6e59f62a1a41e733de3
ZACHARIAH KOVAC <>
2021-05-04 18:02:18 +0000

    commit

--- Lab7-10.cpp
+++ Lab7-10.cpp
@@ -5,6 +5,21 @@ using namespace std;
 int findcross(int [][10]);
 bool checkup(int [][10], int, int);
 bool checkdown(int [][10], int, int);
-bool checkleft
-bool checkright
-bool printout
+bool checkleft (int [][10], int, int);
+bool checkright (int [][10], int, int);
+bool printout (int [][10], int);
+
+int main()
+{
+  int cnt=0;
+  int maze[10][10] =       { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 
+              0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+                             0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+                             0, 1, 0, 0, 1, 0, 0, 0, 0, 0,
+                             1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                             0, 1, 0, 0, 1, 1, 1, 0, 0, 0,
+                             0, 1, 0, 0, 1, 1, 1, 0, 0, 0,
+                             0, 1, 0, 0, 1, 0, 1, 1, 0, 0,
+                             0, 1, 0, 0, 1, 1, 1, 0, 0, 0,
+                             0, 1, 0, 0, 1, 1, 1, 0, 0, 0 }
+}
