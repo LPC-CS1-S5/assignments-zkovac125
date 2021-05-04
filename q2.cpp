7ff5055d01db2f01c065c53e078fe1884474181e
ZACHARIAH KOVAC <>
2021-05-04 19:37:50 +0000

    commit

--- q2.cpp
+++ q2.cpp
@@ -1,27 +1,27 @@
-#inlcude <iostream>
+#include <iostream>
 using namespace std;
 
-void proint triangle(int numbers[][3], int, int);
+void printtriangle(int numbers[][3], int, int);
 int main()
 {
   const int SIZE =3;
   int numbers[SIZE][SIZE] = { {0, 1, 2,}, {3, 4, 5}, {6, 7, 8} };
   int row = 3;
   int col = 3;
-  print triangle(numbers, row, col);
+  printtriangle(numbers, row, col);
 }
-void printtriangle(int num[][3], int row, int col)
+void printtriangle(int num[3][3], int row, int col)
 {
   int i, j;
-  for(int i =0; i <SIZE; i++)
+  for(int i =0; i < row; i++)
   {
-    for(j =0; j < SIZE; j++) {
+    for(j =0; j < col; j++) {
       if (i < j)
       {
         cout << "0" << " ";
       }
       else 
-      cout << numbers[i][j] << " ";
+      cout << num[i][j] << " ";
     } 
     cout << endl;
   }
