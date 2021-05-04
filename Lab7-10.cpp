72398edaa39c79b70159a816d05a5a7256241114
ZACHARIAH KOVAC <>
2021-05-04 17:40:59 +0000

    commit

---
+++ Lab7-10.cpp
@@ -0,0 +1,10 @@
+#inlcude <iostream>
+#include <iomanip>
+using namespace std;
+
+int findcross(int [][10]);
+bool checkup(int [][10], int, int);
+bool checkdown(int [][10], int, int);
+bool checkleft
+bool checkright
+bool printout
