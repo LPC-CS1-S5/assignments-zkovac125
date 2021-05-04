d167358d90e462a0b546471ad4bb9c06c67c3c15
ZACHARIAH KOVAC <>
2021-05-04 20:47:25 +0000

    committ

--- q4.cpp
+++ q4.cpp
@@ -21,7 +21,7 @@ int main()
 }
 void makevector(vector<int> vec)
 {
-  for(int i= 0; i<size; i++)
+  for(int i= 0; i< size; i++)
     vec.push_back(rand() % 10);
 }
 void printvector(vector<int> vec)
