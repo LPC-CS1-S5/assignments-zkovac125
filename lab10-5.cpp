c9a0113fb269c8aeff6dce50eca2c3cd7f88e45a
ZACHARIAH KOVAC <>
2021-05-14 19:49:52 +0000

    committ

--- lab10-5.cpp
+++ lab10-5.cpp
@@ -21,7 +21,27 @@ int	main()
 
 int	splitwords(string txt, char delimiter)
 {
+  int start, found cnt=0;
+  string splitstr;
 
+  start =0;
+  while(found=txt.find(delimiter, start) != string::npos){
+    splitstr = txt.substr(start, found-start);
+    cout << "Split Word " << splitstr << endl;
+    start = found + 1;
+    cnt += 1;
+  }
+  splitstr=txt.substr(start, txt.length()-start);
+    cout << "Split Word " << splitstr << endl;
+    return cnt +1;
+  
+
+
+
+
+  find(',', position)
+  substr(start, found-start)
+  while(find('.'))
 }
 int	findnumchars(string txt)
 {
