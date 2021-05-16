ae64414d7b27934e9f31c4619cf7823aaed10751
ZACHARIAH KOVAC <>
2021-05-16 19:58:58 +0000

    committ

--- Chapter 10 Assignments/lab10-5.cpp
+++ Chapter 10 Assignments/lab10-5.cpp
@@ -21,11 +21,11 @@ int	main()
 
 int	splitwords(string txt, char delimiter)
 {
-  int start, found cnt=0;
+  int start, found, cnt=0;
   string splitstr;
 
   start =0;
-  while(found=txt.find(delimiter, start) != string::npos){
+  while((found=txt.find(delimiter, start)) != string::npos){
     splitstr = txt.substr(start, found-start);
     cout << "Split Word " << splitstr << endl;
     start = found + 1;
@@ -40,20 +40,23 @@ int	findnumchars(string txt) // Count the number of unique characters in the tex
   int seen[26] = {0};
   int index, uniquecnt =0;
   string::iterator it;
-  for(it=text.begin(); it != text.end(); it++){
+  for(it=txt.begin(); it != txt.end(); it++){
     index = *it - 'A';
 	  if(seen[index] == 0){
      seen[index] =1;
-      uniquecnt += 1;
+     uniquecnt += 1;
+     break;
     }
   }
   for(int i=0; i<=26; i++){
     if(seen[i] == 1){
       cout << static_cast<char>(i+'A') << "\t";
+      break;
     }
   }
-  cout << endl;
-  cout << "The number of unique charahcter are " << uniquecnt << endl;
+  cout << "The number of unique character " << uniquecnt << endl;
   return uniquecnt;
+   cout << endl;
 }
 
+//For this code it the main challange was to get it to stop the continious lopp it keep going even after added the break. I needed an extra bracket in the while loop. 
