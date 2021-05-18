d8de854b6c284ef97f0abbcd64a57c4776472cdd
ZACHARIAH KOVAC <>
2021-05-18 20:39:06 +0000

    committ

--- q1.cpp
+++ q1.cpp
@@ -1,20 +1,27 @@
 #include <iomanip>
+#inlucde
 #include <fstream>
 #include <string> 
 
 using namspace std;
+struct Date{
+  string Month;
+  int day;
+  int year;
+
+}
 struct EmployeeRec{
   int ID;
   string first_name;
   string last_name;
   int salary;
-  string department_name
+  string department_name;
+  Date dstart;
 }
-struct Date{
-  string Month;
-  int Day;
-  int Year;
-}
+
+int makestructarray(EmployeeRec [])
+void findEmployee(EmployeeRec [], int, string)
+
 int main
 {
  
