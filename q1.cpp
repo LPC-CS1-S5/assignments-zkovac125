03ea011b75611dc08908392791168658375a4b37
ZACHARIAH KOVAC <>
2021-05-18 20:29:52 +0000

    committ

--- q1.cpp
+++ q1.cpp
@@ -4,15 +4,19 @@
 
 using namspace std;
 struct EmployeeRec{
-
+  int ID;
+  string first_name;
+  string last_name;
+  int salary;
+  string department_name
 }
-
+struct Date{
+  string Month;
+  int Day;
+  int Year;
+}
 int main
 {
-   int ID;
-   string first_name;
-   string last_name;
-   int salary;
-   string department_name;
+ 
    
 }
