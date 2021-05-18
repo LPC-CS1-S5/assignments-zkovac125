ed5bd8d5ae69051c9b9b96b5c9dec33d7e2d3a6e
ZACHARIAH KOVAC <>
2021-05-18 19:50:19 +0000

    Committ

--- lab11-2.cpp
+++ lab11-2.cpp
@@ -5,40 +5,44 @@
 #include	<iostream>
 #include	<fstream>
 #include	<string>
+
 using namespace std;
-const	int 	NUMCOURSE=2;
-struct Score {
+
+const	int 	NUMCOURSE =2;
+struct Scores {
   int score[NUMCOURSE];
   int sum;
   int avg;
 };
-struct Student
+struct Students
 {
 	int		id;
 	string	name;
-  Score screcord;
+  Scores screcord;
 };
-int		makingstructarray(Student []); // return value : the number of struct array
-void	printout(Student s );
-void	findusername(Student [],int, string);
+
+int		makingstructarray(Students []); // return value : the number of struct array
+void	printout(Students s);
+void	findusername(Students [], int, string);
+
 int 	main()
 {
-	int	numofRecords;
-	Student	s[numofRecords];
+	int	N;
+	Students	s[N];
 	string	username;
   int i;
   
-	numofRecords = makingstructarray(s);
-	cout << "Total number of students records are " << numofRecords << endl;
-	for(int	 i=0; i<numofRecords; i++)
-		printout(s[i]);
+	N = makingstructarray(s);
+	cout << "Total number of students records are " << N << endl;
+	for(int	 i=0; i<N; i++)
+		printout(s[N]);
 
 	cout << "Enter user name to find in Student's records\n";
 	cin	>> username;
-	findusername(s, numofRecords, username);
+	findusername(s, N, username);
 
 }
-int		makingstructarray(Student s[])
+int		makingstructarray(Students s[])
 {
   int i;
   ifstream ifs;
@@ -49,24 +53,25 @@ int		makingstructarray(Student s[])
     cerr << "File open failure!";
     exit(0);
   }
-  i =0;
-  while(ifs >> s[i].id >> s[i].name >> s[i].screcord.socre[0] >> s[i]screcord.score[1]){
-    s[i].screcord.sum =0;
+  i = 0;
+  while( ifs >> s[i].id >> s[i].name >> s[i].screcord.score[0] >> s[i].screcord.score[1] )
+  {
+    s[i].screcord.sum = 0;
     for(int idx=0; idx<NUMCOURSE; idx++)
      s[i].screcord.sum += s[i].screcord.score[idx];
-    s[i].screcord.avg =s[i].screcord.sum / NUMCOURSE;
+    s[i].screcord.avg = s[i].screcord.sum / NUMCOURSE;
     i++;
   }
   return i-1;
 }
-void	findusername(Student s[], int numofRecords, string username)
+void	findusername(Students s[], int N, string username)
 {
 	int found =0;
   for(int i=0; i<N; i++)
   {
-    if(s[i].name,find(username) != string::npos){
+    if(s[i].name.find(username) != string::npos){
       cout << "We found the record for" << username << endl;
-      printrecord(s[i]);
+      printout(s[N]);
       found =1;
     }
 
@@ -74,10 +79,13 @@ void	findusername(Student s[], int numofRecords, string username)
   if(!found)
   cout << "We don't have the record for " << username << endl;
 }
-void	printout(Student s)
+void	printout(Students s)
 {
-	cout << s. id << "\t" << s.name << "\t"
-		<< s.score[0] << "\t" << s.score[1] << 
-		"\tsum:" << s.sum << "\taverage: "<< s.avg << endl;
-
+		cout << "ID \t\t Name\t Score1\t Score2\t Sum\t Avg\n";
+		cout << s.id << "\t";
+		cout << s.name << "\t";
+		for(int idx = 0; idx < NUMCOURSE; idx++)
+			cout << s.screcord.score[idx] << "\t\t";
+		cout << s.screcord.sum << "\t\t";
+		cout << s.screcord.avg << endl;
 }
