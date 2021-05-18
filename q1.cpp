3eeb08fc0c9bc3046414996b089002be5059f351
ZACHARIAH KOVAC <>
2021-05-18 20:45:13 +0000

    commit

--- q1.cpp
+++ q1.cpp
@@ -1,9 +1,10 @@
 #include <iomanip>
-#inlucde
+#inlucde <iostream>
 #include <fstream>
 #include <string> 
 
 using namspace std;
+
 struct Date{
   string Month;
   int day;
@@ -19,11 +20,26 @@ struct EmployeeRec{
   Date dstart;
 }
 
-int makestructarray(EmployeeRec [])
+int makerecord(EmployeeRec [])
 void findEmployee(EmployeeRec [], int, string)
+void searchEmployee(EmployeeRec, int, string)
 
 int main
 {
  
    
+}
+int makerecord(EmployeeRec s[])
+{
+  int i;
+  ifstream ifs;
+
+  ifs.open("employee.txt")
+  if(ifs.fail())
+  {
+    cerr << "File Open Error\n";
+    exit(0);
+  }
+  i = 0;
+  while(ifs >> )
 }
