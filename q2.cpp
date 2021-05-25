9b816957caaf94c5a83f31f53cf7e610caa07563
ZACHARIAH KOVAC <>
2021-05-25 15:21:39 +0000

    committ

--- q2.cpp
+++ q2.cpp
@@ -2,3 +2,13 @@
 #inlucde <iostream>
 
 using namespace std;
+
+viod findMaxSum(int[], int[][5], int, int);
+const int SIZE =10;
+int main()
+{
+  const int NUM_Rows =6;
+  const int NUM_COLS =5;
+  int resutl[Size] ={0};
+  int numbers[NUM_ROWS][NUM_COLS]
+}
