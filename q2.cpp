3dc88b4e1760a6b5487da8d3e52b951d60540337
ZACHARIAH KOVAC <>
2021-05-25 15:25:27 +0000

    committ

--- q2.cpp
+++ q2.cpp
@@ -7,8 +7,17 @@ viod findMaxSum(int[], int[][5], int, int);
 const int SIZE =10;
 int main()
 {
-  const int NUM_Rows =6;
+  const int NUM_ROWS =6;
   const int NUM_COLS =5;
   int resutl[Size] ={0};
-  int numbers[NUM_ROWS][NUM_COLS]
+  int numbers[NUM_ROWS][NUM_COLS] = {{2, 7, 9, 6, 4,}
+  {6, 1, 8, 10, 4}
+  {4, 3, 7, 2, 9}
+  {9, 9, 0, 3, 1}
+  {8, 8, 7, 8, 9}
+  {1, 2, 1, 2, 3}};
+  findMaxSum(result, numner, NUM_ROWS, NUM_COLS);
+  cout << "The row vlaues are ";
+  for(int i=0; i<rows; i++)
+  }
 }
