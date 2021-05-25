fb00f935e2dd6901ddc94f570d59ff141eeda625
ZACHARIAH KOVAC <>
2021-05-25 16:04:17 +0000

    committ

---
+++ q3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  const int SIZE1 =5;
+  const int SIZE2 =10;
+
+  int array1[]={2,3,5,9.10};
+  int array2[]={1,2,3,5,9,10,11,13,15,1};
+
+  //looks for the emenet of the first array in the second arry analyzing one-by-one. 
+  bool found = flase;
+    for()
+}
