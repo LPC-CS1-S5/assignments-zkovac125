cfa3b56996074a13c65bca2578bfd747f82ffabc
ZACHARIAH KOVAC <>
2021-05-25 16:28:38 +0000

    committ

--- q5.cpp
+++ q5.cpp
@@ -1,12 +1,11 @@
-#include <iostrem>
+#include <iostream>
 #include <fstream>
 #include <iomanip>
 
 using namespace std;
-const namespace std;
 
-const unsingned short N=5;
-struct Scires{
+const unsigned short N=5;
+struct Scores{
   double sc[N];
   string grade;
 };
@@ -21,21 +20,21 @@ void printstruct(Grade);
 int main()
 {
   ifstream ifs;
-  ifs.open("grades.bin")'
+  ifs.open("grades.bin");
   if(! ifs)
   {
-    cout << "File Open Error"'
+    cout << "File Open Error";
     exit(0);
   }
   while(ifs.read((char *)&g, sizeof(g))){
     printstruct(g);
   }
 }
-void pritnstruct(Grade g)
+void printstruct(Grade g)
 {
   cout << "Name: " << g.first << g.last << "\t" << "SSN: " << g.ssn << "\n";
-  coutn<< "t\Scores: ";
+  cout << "\tScores: " ;
   for(int i=0; i<N; i++)
-    cout << showpoint << setpercision(2) << g/socre.sc[i] << "\t";
-      cout << "Grade " << g.socre.grade << endl;
+    cout << showpoint << setprecision(2) << g.score.sc[i] << "\t";
+      cout << "Grade " << g.score.grade << endl;
 }
